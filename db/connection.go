aab8d86554f338f11bd4cbb8e637d750f911c74c
DanilsGit <danielestebanmarquez12@gmail.com>
2024-07-16 17:26:33 -0500

    connection with vercel

--- db/connection.go
+++ db/connection.go
@@ -1,17 +1,32 @@
 package db
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DSN = "host=localhost user=gymstats password=123 dbname=gymstatsdb port=7000 "
-
 var DB *gorm.DB
 
 func DBConnection() {
+
+	// Carga las variables de entorno desde el archivo .env
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error cargando el archivo .env")
+	}
+
+	// Construye el DSN usando las variables de entorno
+	DSN := "host=" + os.Getenv("POSTGRES_HOST") +
+		" user=" + os.Getenv("POSTGRES_USER") +
+		" password=" + os.Getenv("POSTGRES_PASSWORD") +
+		" dbname=" + os.Getenv("POSTGRES_DATABASE") +
+		" port=5432"
+
 	var error error
 	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
 	if error != nil {
